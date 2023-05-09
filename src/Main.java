05752942e44290727a72f0a6adef23fd0ccf81a9
sthompson <thompson.sawyer.m@gmail.com>
2023-05-09 13:34:53 -0700

    Added instance variables and constructor

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    private int count;
+    private String name;
+
+    public Main(int count, String name) {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
